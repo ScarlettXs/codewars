7c56e1c2dde1b137da9b446d157dd85bb2cd9a13
ScarlettXs <wailiandxs@gmail.com>
2024-07-08 20:17:08 +0200

    add sum list but ignore duplicates kata solution

---
+++ sum_a_list_but_ignore_any_duplicates/solution.py
@@ -0,0 +1,12 @@
+def sum_no_duplicates(l):
+    l.sort()
+    reduce_list = []
+    final_list = []
+    for i in range(len(l)-1):
+        if l[i] == l[i+1]:
+            reduce_list.append(l[i])
+    for item in l:
+        if item not in reduce_list:
+            final_list.append(item)
+    result = sum(final_list)
+    return result
